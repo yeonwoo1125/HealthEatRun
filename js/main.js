a2f4d28d76f712483cd30fe597c871646e76fca3
yeonwoo1125 <s2021@e-mirim.hs.kr>
2021-11-07 16:38:26 +0900

    main 버튼 생성 및 위치 수정

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 //메인 부분 
-const btn_gameStart =document.getElementsByClassName("gameStart");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+/*var user={
+    x :100,
+    y:500,
+    width:50,
+    height:50,
+
+}*/
