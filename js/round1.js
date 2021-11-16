bb42c2f17f1e08063276269c11dd30fd046fb9b7
yeonwoo1125 <s2021@e-mirim.hs.kr>
2021-11-16 23:14:41 +0900

    round 1 스타일

--- js/round1.js
+++ js/round1.js
@@ -2,27 +2,49 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 
-//달리는 객체
-var user={
-    x :100,
-    y:500,
-    width:50,
-    height:50,
+//사용할 캔버스 크기 지정
+canvas.width = window.innerWidth ;
+canvas.height = window.innerHeight-105 ;
 
+//player객체, 라운드마다 달라질 수 있음
+class player{
+    constructor(){
+        this.x = 50;
+        this.y = canvas.height;
+        this.width = 100;
+        this.height = 100;
+    }
+    draw(){
+        ctx.fillStyle="red";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
 
 //장애물 객체
 class food{
-    constructor(x,y,height, width) {
-        this.x=x;
-        this.y=y;
-        this.height = height;
-        this.width = width;
+    constructor() {
+        this.x = canvas.width - 50;
+        this.y = 520;
+        this.width = 60;
+        this.height = 70;
       }
+      draw(){
+        ctx.fillStyle="blue";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
 
-function drawFood(){
+var jumping=false;
+//점프 방향키 이벤트
+document.addEventListener('keydown',function(e){ //이벤트리스너 : 특정 이벤트가 발생하면 실행하는 함수
+    if(e.code==='Space' || e.code === 'ArrowUp'){ //스페이스 또는 위쪽 방향키를 누르면 점프함
+        jumping = true;
+    }
+})
 
-}
-//10밀리초 마다 함수 실행
-setInterval(drawFood,10);
+var sliding=false;
+document.addEventListener('keydown',function(e){ //이벤트리스너 : 특정 이벤트가 발생하면 실행하는 함수
+    if(e.code==='ArrowDown'){ //스페이스 또는 위쪽 방향키를 누르면 점프함
+        sliding = true;
+    }
+})
