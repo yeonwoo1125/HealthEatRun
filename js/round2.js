12dfbe1772b0d7cd0143d6b890bf04ea756b726c
yeonwoo1125 <s2021@e-mirim.hs.kr>
2021-11-18 22:18:27 +0900

    끝난 후 다음으로 버튼 생성

--- js/round2.js
+++ js/round2.js
@@ -10,28 +10,59 @@ var clickList =['','']; //클릭한 아이디 두개 저장
 var foodList=['salad1','steak1','shake1','oatmeal1','chickenBreast1','tofu1','milk1', 
             'salad2','steak2','shake2','oatmeal2','chickenBreast2','tofu2','milk2']; //음식 이름 저장
 var ranArr = new Array(); //랜덤 수 
+var chk = new Array(); //가져온 아이디를 2개 저장
 
 setId();  //div 요소에 id 생성
-setImg();
 
 for(var i = 0; i < cardLen; i++){
+   
     card[i].addEventListener("click",()=>{ //card 클래스를 가진 div에 클릭 이벤트 부여
         clickCnt++;
         selCnt++;
-        alert('카드 클릭');
-        if(selCnt == 1) clickList[0] = clickId; //클릭한 카드의 아이디를 배열에 저장
-        else if(selCnt == 2) clickList[1] = clickId;
+        if(selCnt == 1) {
+            clickList[0] = clickId; //클릭한 카드의 아이디를 배열에 저장
+        }
+        else if(selCnt == 2) {
+            clickList[1] = clickId;
+        }
 
         if(selCnt == 2 && clickList[0] == clickList[1]){
+            score++;
+            createNextBtn();
             reset();
-            alert("짝 맞춤");
+            document.getElementById(chk[0]).style.backgroundColor = 'red';
+            document.getElementById(chk[1]).style.backgroundColor = 'red';
+            chk.splice(0,2);
         } 
-        else if(selCnt==2){
+        else if(selCnt == 2){
             reset();
-            alert("짝 아님");
+            setTimeout(function(){
+                document.getElementById(chk[0]).style.background = "url('')";
+                document.getElementById(chk[1]).style.background = "url('')";
+                document.getElementById(chk[0]).style.backgroundColor = 'aqua';
+                document.getElementById(chk[1]).style.backgroundColor = 'aqua';
+                document.getElementsByClassName('card').style
+                chk.splice(0,2);
+            },1000);
         } 
+
     });
 };
+
+//게임이 끝난 후 다음으로 이동하는 버튼 만들기
+function createNextBtn(){
+    if(score == 7){
+        var linkRound3 = document.createElement('a');
+        linkRound3.href='../html/round3.html';
+        document.body.appendChild(linkRound3);
+    
+        var nextBtn = document.createElement('button');
+        nextBtn.setAttribute('id','next_btn');
+        nextBtn.innerHTML ="다음으로";
+        linkRound3.appendChild(nextBtn);
+    }
+};
+
 //값들 초기화
 function reset(){
     selCnt = 0;
@@ -39,11 +70,64 @@ function reset(){
     clickList[1] ='';
 };
 
-function sendId(click_id){
-    console.log(click_id);
+//카드 선택 시 이미지 나타남
+function clickCard(click_id){
+
+    chk.push(click_id);
     clickId = click_id.slice(0,-1);
     //선생님한테 물어보기
     //document.getElementById(click_id).style.background = "url('../img/round2/'+clickId+'.png') no-repeat center center";
+
+    if(click_id =="salad1"){
+        document.getElementById('salad1').style.background = "url('../img/round2/salad.png') no-repeat center center";
+        document.getElementById('salad1').style.backgroundSize = "230px 230px";
+    }
+    else if(click_id =="salad2"){
+        document.getElementById('salad2').style.background = "url('../img/round2/salad.png') no-repeat center center";
+        document.getElementById('salad2').style.backgroundSize = "230px 230px";
+    }
+    else if(click_id =="steak1"){
+        document.getElementById('steak1').style.background = "url('../img/round2/steak.png') no-repeat center center";
+    }
+    else if(click_id =="steak2"){
+        document.getElementById('steak2').style.background = "url('../img/round2/steak.png') no-repeat center center";
+    }
+    else if(click_id =="oatmeal1"){
+        document.getElementById('oatmeal1').style.background = "url('../img/round2/oatmeal.png') no-repeat center center";
+    }
+    else if(click_id =="oatmeal2"){
+        document.getElementById('oatmeal2').style.background = "url('../img/round2/oatmeal.png') no-repeat center center";
+    }
+    else if(click_id =="tofu1"){
+        document.getElementById('tofu1').style.background = "url('../img/round2/tofu.png') no-repeat center center";
+    }
+    else if(click_id =="tofu2"){
+        document.getElementById('tofu2').style.background = "url('../img/round2/tofu.png') no-repeat center center";
+    }
+    else if(click_id =="milk1"){
+        document.getElementById('milk1').style.background = "url('../img/round2/milk.png') no-repeat center center";
+        document.getElementById('milk1').style.backgroundSize = "200px 230px";
+    }
+    else if(click_id =="milk2"){
+        document.getElementById('milk2').style.background = "url('../img/round2/milk.png') no-repeat center center";
+        document.getElementById('milk2').style.backgroundSize = "200px 230px";
+    }
+    else if(click_id == "chickenBreast1"){
+        document.getElementById('chickenBreast1').style.background = "url('../img/round2/chikenBreast.png') no-repeat center center";
+        document.getElementById('chickenBreast1').style.backgroundSize = "200px 100px";
+    }
+    else if(click_id == "chickenBreast2"){
+        document.getElementById('chickenBreast2').style.background = "url('../img/round2/chikenBreast.png') no-repeat center center";
+        document.getElementById('chickenBreast2').style.backgroundSize = "200px 100px";
+    }
+    else if(click_id == "shake1"){
+        document.getElementById('shake1').style.background = "url('../img/round2/shake.png') no-repeat center center";
+        document.getElementById('shake1').style.backgroundSize = "100px 200px";
+    }
+    else if(click_id == "shake2"){
+        document.getElementById('shake2').style.background = "url('../img/round2/shake.png') no-repeat center center";
+        document.getElementById('shake2').style.backgroundSize = "100px 200px";
+    }
 };
 
 //div에 아이디 부여
@@ -71,37 +155,4 @@ function ran(){
         ranArr[i] = ranArr[n];
         ranArr[n] = tmp;
     }
-};
-
-//id에 맞는 이미지 부여하기
-function setImg(){
-    //div의 id를 가져와서 if문으로 이미지 주기
-    document.getElementById('salad1').style.background = "url('../img/round2/salad.png') no-repeat center center";
-    document.getElementById('salad1').style.backgroundSize = "230px 230px";
-    document.getElementById('salad2').style.background = "url('../img/round2/salad.png') no-repeat center center";
-    document.getElementById('salad2').style.backgroundSize = "230px 230px";
-
-    document.getElementById('steak1').style.background = "url('../img/round2/steak.png') no-repeat center center";
-    document.getElementById('steak2').style.background = "url('../img/round2/steak.png') no-repeat center center";
-
-    document.getElementById('oatmeal1').style.background = "url('../img/round2/oatmeal.png') no-repeat center center";
-    document.getElementById('oatmeal2').style.background = "url('../img/round2/oatmeal.png') no-repeat center center";
-
-    document.getElementById('tofu1').style.background = "url('../img/round2/tofu.png') no-repeat center center";
-    document.getElementById('tofu2').style.background = "url('../img/round2/tofu.png') no-repeat center center";
-
-    document.getElementById('milk1').style.background = "url('../img/round2/milk.png') no-repeat center center";
-    document.getElementById('milk1').style.backgroundSize = "200px 230px";
-    document.getElementById('milk2').style.background = "url('../img/round2/milk.png') no-repeat center center";
-    document.getElementById('milk2').style.backgroundSize = "200px 230px";
-
-    document.getElementById('chickenBreast1').style.background = "url('../img/round2/chikenBreast.png') no-repeat center center";
-    document.getElementById('chickenBreast1').style.backgroundSize = "200px 100px";
-    document.getElementById('chickenBreast2').style.background = "url('../img/round2/chikenBreast.png') no-repeat center center";
-    document.getElementById('chickenBreast2').style.backgroundSize = "200px 100px";
-
-    document.getElementById('shake1').style.background = "url('../img/round2/shake.png') no-repeat center center";
-    document.getElementById('shake1').style.backgroundSize = "100px 200px";
-    document.getElementById('shake2').style.background = "url('../img/round2/shake.png') no-repeat center center";
-    document.getElementById('shake2').style.backgroundSize = "100px 200px";
 };
