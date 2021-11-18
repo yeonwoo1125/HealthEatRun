ae092fe03a13f378e1004e322bd81fbd50bc39b5
yoonjuhee22 <s2014@e-mirim.hs.kr>
2021-11-18 15:59:42 +0900

    캔버스 생성 및 캐릭터 생성

--- js/round1.js
+++ js/round1.js
@@ -1,57 +1,24 @@
-const canvas=document.getElementById('canvas');
-const ctx = document.getElementById('2d');
+//캔버스 생성
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
-const horizontal = 5; //가로
-const vertical = 4; //세로
+canvas.width = window.innerwindow-100;
+canvas.height = window.innerHeight-100;
 
-//사용할 캔버스 크기 지정
-canvas.width = window.innerWidth ;
-canvas.height = window.innerHeight-105 ;
+ctx.fillStyle = 'green';
+ctx.fillRect(10,10,100,100);
 
-//player객체, 라운드마다 달라질 수 있음
-class player{
-    constructor(){
-        this.x = 50;
-        this.y = canvas.height;
-        this.width = 100;
-        this.height = 100;
-    }
+//달리는 캐릭터 
+var character = {
+    x:10,
+    y:200,
+    width: 50,
+    
+    //draw 메소드
     draw(){
-        ctx.fillStyle="red";
+        ctx.fillStyle = 'green';
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
-//장애물 객체
-class food{
-    constructor() {
-        this.x = canvas.width - 50;
-        this.y = 520;
-        this.width = 60;
-        this.height = 70;
-      }
-      draw(){
-        ctx.fillStyle="blue";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-}
-
-var jumping=false;
-//점프 방향키 이벤트
-document.addEventListener('keydown',function(e){ //이벤트리스너 : 특정 이벤트가 발생하면 실행하는 함수
-    if(e.code==='Space' || e.code === 'ArrowUp'){ //스페이스 또는 위쪽 방향키를 누르면 점프함
-        jumping = true;
-    }
-})
-
-var sliding=false;
-document.addEventListener('keydown',function(e){ //이벤트리스너 : 특정 이벤트가 발생하면 실행하는 함수
-    if(e.code==='ArrowDown'){ //스페이스 또는 위쪽 방향키를 누르면 점프함
-        sliding = true;
-    }
-})
-
-//.document.querySelector()는 css선택자. container를 검색(요소를 검색하는것)
-const container = document.querySelector(".container"); 
-
-
+character.draw();
