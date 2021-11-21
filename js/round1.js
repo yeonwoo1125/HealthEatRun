28288338ed009ebc59477043642fecd0040c8e68
yoonjuhee22 <s2014@e-mirim.hs.kr>
2021-11-21 20:17:48 +0900

    장애물, 캐릭터 위치 조정

--- js/round1.js
+++ js/round1.js
@@ -13,9 +13,9 @@ img2.src = 'hamburger.png';
 
 //달리는 캐릭터 
 var character = {
-    x:170,
-    y:300,
-    width:50,
+    x:200,
+    y:500,
+    width:80,
     height : 100, 
 
     //draw 메소드
@@ -32,10 +32,10 @@ var character = {
 class Food{
     constructor(){ 
         //크기는 캐릭터와 동일하지만 위치는 다르게 한다
-        this.x = 800;
-        this.y = 250;
+        this.x = window.innerWidth-50; 
+        this.y = 450;
         this.width = 70;
-        this.height = 50;
+        this.height = 70;
     }
     //draw 메소드
     draw(){
@@ -61,7 +61,7 @@ function frame(){ //프레임마다 실행을 할 함수
 
     ctx.clearRect(0,0, canvas.width, canvas.height); //canvas의 context안에 존재하는 메소드. x,y를 0으로 설정하면 Canvas 전체 영역을 지우는 것이 됨. 즉, 물체가 남지 않고 이동하게
 
-    if(timer%120==0){ //120프레임마다 한번 움직이게 하기
+    if(timer%200==0){ //120프레임마다 한번 움직이게 하기
         var food = new Food();
         foodmix.push(food); //foodmix라는 배열에 120 프레임마다 한번씩 food를 푸시.(배열이 점점 차오른다)
     }
@@ -83,17 +83,17 @@ function frame(){ //프레임마다 실행을 할 함수
 
     //character.y-=2; 를
     if(jump==true){ //jump값이 true가 된다면(space를 누른다면)
-        character.y-=2; //y가 쭉 올라가게 한다
+        character.y--; //y가 쭉 올라가게 한다
         jumpTime+=2; //jumpTime도 증가
     }
 
     if(jump==false){
-        if(character.y<230){  //y축의 위치가 일정높이에 다다랐을때
-            character.y+=2; //아래로 내려오게(y값을 늘린다)
+        if(character.y<400){  //y축의 위치가 일정높이에 다다랐을때
+            character.y++; //아래로 내려오게(y값을 늘린다)
         }
     }
 
-    if(jumpTime>30){ //jumpTime이 100프레임을 넘긴다면 
+    if(jumpTime>100){ //jumpTime이 50프레임을 넘긴다면 
         jump=false; //멈추기(y축의 이동을)
         //여기까지만 하면 멈추기만 하고 다시 jump기능이 작동을 안한다
         jumpTime=0; //그래서 jumptime을 초기화했다
