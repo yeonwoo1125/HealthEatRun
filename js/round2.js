ede58d7c7ffbec620c46aac192a62dfd352fc028
yeonwoo1125 <s2021@e-mirim.hs.kr>
2021-11-30 02:56:26 +0900

    round2 삭제 및 round3 파일들 이름 변경->round2

--- js/round2.js
+++ js/round2.js
@@ -1,200 +1,248 @@
-//카드 클래스를 가진 엘리먼트 모두 가져오기
-const card = document.querySelectorAll(".card");
+let score = 0;
+if(localStorage.getItem("score")){
+    score = Number(localStorage.getItem("score"));
+}
+function ranking(){ //모든 값을 가져와서 저장한 후 localStorage를 clear 하고 최대값 3개를 구하고 다시 넣기
+    let scoreLen = localStorage.length; //랭크 안에 들어있는 점수의 길이, 0~3
 
-let score = 0; //짝을 맞춘 갯수
-let selCnt = 0; //현재 뒤집힌 카드 갯수
-let clickCnt = 0; //카드 총선택 횟수
-const cardLen = card.length; //카드의 수
-let clickId; //클릭한 카드의 아이디값을 가져옴
-let clickList = ['','']; //클릭한 아이디 두개 저장
-let foodList = ['salad1','steak1','shake1','oatmeal1','chickenBreast1','tofu1','milk1', 
-            'salad2','steak2','shake2','oatmeal2','chickenBreast2','tofu2','milk2']; //음식 이름 저장
-let ranArr = new Array(); //랜덤 수 
-let chk = new Array(); //가져온 아이디를 2개 저장
+}
+let disc = document.createElement('div');
+disc.setAttribute('id','disc');
+disc.innerHTML = "Space를 눌러 게임을 시작하세요!";
+document.body.appendChild(disc);
 
-const currentCard = new Audio(); // Aduio 객체 생성 \
-currentCard.src = "../music/띠링.mp3"; // 음원 파일 설정
+const background = new Audio('../music/SnakeontheBeach.mp3'); //배경 음악
+background.volume = 0.1;
+let start = false;
 
-let timer;
-setId();
-for(let i = 0; i < cardLen; i++){
-    card[i].addEventListener("click",()=>{ //card 클래스를 가진 div에 클릭 이벤트 부여
-        //document.getElementById(this).setAttribute('disabled','disabled');
-        nextRound();
-        endRound();
-        clickCnt++;
-        selCnt++;
+let bottom = document.createElement('div')
+bottom.setAttribute('id','bottom');
+document.body.appendChild(bottom);
 
-        //클릭한 카드의 아이디를 배열에 저장
-        if(selCnt == 1) {
-            clickList[0] = clickId; 
+const canvas = document.createElement('canvas');
+canvas.setAttribute('id','canvas');
 
-        }
-        else if(selCnt == 2) {
-            clickList[1] = clickId;
-        }
 
-        //카트 선택한 후
-        if(selCnt == 2 && clickList[0] == clickList[1]){
-            currentCard.play(); // 음원 재생 
-            score++;
-            reset();
-            document.getElementById(chk[0]).style.backgroundColor = '#D12F2C';
-            document.getElementById(chk[1]).style.backgroundColor = '#D12F2C';
-            document.getElementById(chk[0]).setAttribute("disabled", "disabled");
-            document.getElementById(chk[1]).style.backgroundColor = '#D12F2C';
-            document.getElementById(chk[1]).setAttribute("disabled", "disabled");
-            chk.splice(0,2);
-            nextRound();
-        } 
-        else if(selCnt == 2){
-            reset();
-            setTimeout(function(){
-                document.getElementById(chk[0]).style.backgroundColor = '#734434';
-                document.getElementById(chk[0]).style.background = "url('')";
-                document.getElementById(chk[1]).style.background = "url('')";
-                document.getElementById(chk[0]).style.backgroundColor = '#734434';
-                document.getElementById(chk[1]).style.backgroundColor = '#734434';
-                chk.splice(0,2);
-            },1300);
-        } 
-    });
-};
+const ctx = canvas.getContext('2d');
 
-//게임이 끝난 후 다음 라운드로 이동
-function nextRound(){
-    if(score == 7){
-        let newDiv = document.createElement('div');
-        newDiv.setAttribute('id','completeRound');
-        newDiv.innerHTML ="GO NEXT ROUND!";
+//canvas 크기
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight-105;
 
-        document.body.appendChild(newDiv);
+let runningman = new Image();
+runningman.src="../img/round2/man_right.png";
 
-        setTimeout(function(){ //2초 후 페이지 이동
-            location.href = "../html/round3.html";
-        } ,2000);
+//배열에 넣어서 랜덤으로 나오게 하기
+const fishBread = new Image();
+fishBread.src = "../img/round2/fishBread.png";
+const eggBread = new Image();
+eggBread.src = "../img/round2/eggBread.png";
+const chikenSkewers = new Image();
+chikenSkewers.src = "../img/round2/chikenSkewers.png";
+const fishCake = new Image();
+fishCake.src = "../img/round2/fishCake.png";
+const icecream = new Image();
+icecream.src = "../img/round2/icecream.png";
+//이미지 객체 배열에 넣기
+const dropFoodList = new Array(fishBread,eggBread,chikenSkewers,fishCake,icecream);
+
+function ready(){
+    disc.remove();
+    document.body.appendChild(canvas);
+    start = true;
+
+    //background.play();
+    progressBar();
+    startGame(); //space를 한번 눌러야 시작
+}
+
+//움직이는 player obj
+let player = {
+    x : canvas.width/2-75,
+    y : 669,
+    width : 150,
+    height : 150,
+    draw(){
+        ctx.drawImage(runningman, this.x, this.y, this.width, this.height);     
     }
 };
 
-//게임 실패 후 띄워지는 버튼
-function endRound(){
-    if(clickCnt == 20){
-        let newDiv = document.createElement('div');
-        newDiv.setAttribute('id','failRound');
-        newDiv.innerHTML ="GAME OVER";
-        document.body.appendChild(newDiv);
-
-        let btnDiv = document.createElement('div');
-        btnDiv.setAttribute('id','btnDiv');
-        document.body.appendChild(btnDiv);
-
-        let mainBtn = document.createElement('button');
-        mainBtn.setAttribute('id','goMainBtn');
-        mainBtn.innerHTML = "Main";
-        mainBtn.addEventListener('click',()=>{
-            location.href = "../html/main.html";
-        });
-        btnDiv.appendChild(mainBtn);
-
-        let retryBtn = document.createElement('button');
-        retryBtn.setAttribute('id','retryBtn');
-        retryBtn.addEventListener('click',()=>{
-            location.href = "../html/round1.html";
-        });
-        retryBtn.innerHTML = "Retry";
-        btnDiv.appendChild(retryBtn);
-
+//하늘에서 떨어지는 음식
+class Food{
+    constructor(){
+        this.x = Math.floor(Math.random()*canvas.width); 
+        this.y = 0;
+        this.width = 80;
+        this.height = 70;
+        this.speed = Math.floor(Math.random()*17+3); //떨어지는 속도
+        this.foodImg= dropFoodList[Math.floor(Math.random()*5)]; //떨어지는 음식 
+    }
+    draw(){   //이미지 그림
+        ctx.drawImage(this.foodImg, this.x, this.y, this.width, this.height)
     }
 }
 
-//카드에 아이디 부여
-function setId(){
-    ran();
-    for(let i=0; i<14; i++){
-        card[i].setAttribute('id',foodList[ranArr[i]]);
-    }
-}
-//값들 초기화
-function reset(){
-    selCnt = 0;
-    clickList[0] ='';
-    clickList[1] ='';
-};
-
-function clickCard(click_id){
-    chk.push(click_id); //뒷자리 안잘린 아이디
-    clickId = click_id.slice(0,-1);
-
-    //document.getElementById(click_id).style.background = `url('../img/round2/'${clickId}'.png') no-repeat center center`;
-   
-    if(click_id =="salad1"){
-        document.getElementById('salad1').style.background = "url('../img/round2/salad.png') no-repeat center center";
-        document.getElementById('salad1').style.backgroundSize = "230px 230px";
-    }
-    else if(click_id =="salad2"){
-        document.getElementById('salad2').style.background = "url('../img/round2/salad.png') no-repeat center center";
-        document.getElementById('salad2').style.backgroundSize = "230px 230px";
-    }
-    else if(click_id =="steak1"){
-        document.getElementById('steak1').style.background = "url('../img/round2/steak.png') no-repeat center center";
-    }
-    else if(click_id =="steak2"){
-        document.getElementById('steak2').style.background = "url('../img/round2/steak.png') no-repeat center center";
-    }
-    else if(click_id =="oatmeal1"){
-        document.getElementById('oatmeal1').style.background = "url('../img/round2/oatmeal.png') no-repeat center center";
-    }
-    else if(click_id =="oatmeal2"){
-        document.getElementById('oatmeal2').style.background = "url('../img/round2/oatmeal.png') no-repeat center center";
-    }
-    else if(click_id =="tofu1"){
-        document.getElementById('tofu1').style.background = "url('../img/round2/tofu.png') no-repeat center center";
-    }
-    else if(click_id =="tofu2"){
-        document.getElementById('tofu2').style.background = "url('../img/round2/tofu.png') no-repeat center center";
-    }
-    else if(click_id =="milk1"){
-        document.getElementById('milk1').style.background = "url('../img/round2/milk.png') no-repeat center center";
-        document.getElementById('milk1').style.backgroundSize = "200px 230px";
-    }
-    else if(click_id =="milk2"){
-        document.getElementById('milk2').style.background = "url('../img/round2/milk.png') no-repeat center center";
-        document.getElementById('milk2').style.backgroundSize = "200px 230px";
-    }
-    else if(click_id == "chickenBreast1"){
-        document.getElementById('chickenBreast1').style.background = "url('../img/round2/chikenBreast.png') no-repeat center center";
-        document.getElementById('chickenBreast1').style.backgroundSize = "200px 100px";
-    }
-    else if(click_id == "chickenBreast2"){
-        document.getElementById('chickenBreast2').style.background = "url('../img/round2/chikenBreast.png') no-repeat center center";
-        document.getElementById('chickenBreast2').style.backgroundSize = "200px 100px";
-    }
-    else if(click_id == "shake1"){
-        document.getElementById('shake1').style.background = "url('../img/round2/shake.png') no-repeat center center";
-        document.getElementById('shake1').style.backgroundSize = "100px 200px";
-    }
-    else if(click_id == "shake2"){
-        document.getElementById('shake2').style.background = "url('../img/round2/shake.png') no-repeat center center";
-        document.getElementById('shake2').style.backgroundSize = "100px 200px";
+let esc = false;
+//키보드 이벤트
+document.addEventListener('keydown',(e)=>{
+    if(e.code === "Space"){
+        if(start === false)
+         ready();
     } 
-};
+    if(e.code === "Escape") {
+        if(esc === true) {
+            startGame(); //esc 누른 후 다시 눌렀을 때 애니메이션 시작
+            //background.play();
+            esc = false;
+        }
+        else {
+            cancelAnimationFrame(animation); //esc를 누르면 애니메이션 정지
+            background.pause();
+            esc = true;
+        }
+    }
+    if(e.code === "ArrowRight") {
+        if(esc === false){
+            player.x += 30;
+            runningman.src = "../img/round2/man_right.png";
+            if(player.x > canvas.width) player.x = canvas.width
+        }
+    }
+    if(e.code === "ArrowLeft") {
+        if(esc === false){
+            player.x -= 30;
+            runningman.src="../img/round2/man_left.png";
+            if(player.x < 0) player.x = 0
+        }
+    }
+});
 
-//랜덤 수 중복제거
-function ran(){
-    let tmp;
-    let n;
-   
-    //전달받은 매개변수 n만큼 배열  
-    for(let i=0; i<14; i++){
-        ranArr.push(i);
+let animation;
+let foodList=[]; //food들을 가지고 있음
+let frameCnt = 0; //프레임 실행 횟수
+let progressWidth = 0; //progress바의 게이지 
+//게임 시작
+function startGame() {
+    animation = requestAnimationFrame(startGame)
+    frameCnt++;
+
+    if(frameCnt % 100 === 0) progressWidth+=4;
+    if(progressWidth<= 100) progress.setAttribute('value',progressWidth);
+    else finishRound();
+
+    ctx.clearRect(0,0, canvas.width, canvas.height); //canvas 초기화
+
+    if(frameCnt % 100 === 0){
+        score++;
+        localStorage.setItem("score",score);
+    } 
+    if(frameCnt % 15 === 0){ //180프레임 마다 장애물 그림
+        let food = new Food();
+        foodList.push(food);
     }
 
-    //값을 서로 섞기
-    for(let i=0; i< ranArr.length ; i++){
-        n = Math.floor(Math.random() *14); 
-        tmp = ranArr[i];
-        ranArr[i] = ranArr[n];
-        ranArr[n] = tmp; 
+    foodList.forEach((f,i,fl)=>{
+        //장애물의 y좌표가 0보다 작을 시 제거함
+        if(f.y > canvas.height) fl.splice(i,1);
+
+        //좌표 생성 위치는 랜덤, 랜덤한 속도로 바닥에 닿음
+        f.y += f.speed;
+
+        chkCollison(player,f); //모든 장애물에 대해 충돌체크
+        f.draw();
+    });
+
+    player.draw();
+}
+//모든 라운드를 끝냄
+function finishRound(){ 
+    cancelAnimationFrame(animation);
+    background.pause();
+
+    let gameClearDiv = document.createElement('div');
+    gameClearDiv.setAttribute('id','gameClearDiv');
+    gameClearDiv.innerHTML = "GAME CLEAR!";
+    document.body.appendChild(gameClearDiv);
+
+    let btnDiv = document.createElement('div');
+    btnDiv.setAttribute('id','btnDiv');
+    document.body.appendChild(btnDiv);
+
+    let scoreDiv = document.createElement('div');
+    scoreDiv.setAttribute('id','scoreDiv');
+    scoreDiv.innerHTML="SCORE : "+score+"kcal";
+    document.body.appendChild(scoreDiv);
+
+    let mainBtn = document.createElement('button');
+    mainBtn.setAttribute('id','goMainBtn');
+    mainBtn.innerHTML = "MAIN";
+    mainBtn.addEventListener('click',()=>{
+        location.href = "../html/main.html";
+    });
+     btnDiv.appendChild(mainBtn);
+
+    let rankingBtn = document.createElement('button');
+    rankingBtn.setAttribute('id','rankingBtn');
+    rankingBtn.innerHTML = "RANK";
+    rankingBtn.addEventListener('click',()=>{
+        location.href = "../html/ranking.html";
+    });
+    btnDiv.appendChild(rankingBtn);
+}
+
+//죽었을 때 보여주는 화면
+function endRound(){
+    cancelAnimationFrame(animation);
+    background.pause();
+    let gameOverDiv = document.createElement('div');
+    gameOverDiv.setAttribute('id','failRound');
+    gameOverDiv.innerHTML ="GAME OVER";
+    document.body.appendChild(gameOverDiv);
+
+    let btnDiv = document.createElement('div');
+    btnDiv.setAttribute('id','btnDiv');
+    document.body.appendChild(btnDiv);
+
+    let scoreDiv = document.createElement('div');
+    scoreDiv.setAttribute('id','scoreDiv');
+    scoreDiv.innerHTML="SCORE : "+score+"kcal";
+    document.body.appendChild(scoreDiv);
+
+    let mainBtn = document.createElement('button');
+    mainBtn.setAttribute('id','goMainBtn');
+    mainBtn.innerHTML = "MAIN";
+    mainBtn.addEventListener('click',()=>{
+        location.href = "../html/main.html";
+    });
+     btnDiv.appendChild(mainBtn);
+
+    let retryBtn = document.createElement('button');
+    retryBtn.setAttribute('id','retryBtn');
+    retryBtn.addEventListener('click',()=>{
+        location.href = "../html/round1.html";
+    });
+    retryBtn.innerHTML = "RETRY";
+    btnDiv.appendChild(retryBtn);
+}
+
+const progress = document.createElement('progress')
+//게임의 진행도를 나타냄
+function progressBar(){
+    progress.setAttribute('id','progress');
+    progress.setAttribute('Max',100);
+    progress.setAttribute('value',0);
+    document.body.appendChild(progress);
+}
+
+//음식과 player의 충돌체크
+function chkCollison(player, food) {
+    if(food.y >= player.y+player.height && (food.x >= player.x && food.x<=player.x+player.width)) {
+        ctx.clearRect(0,0, canvas.width, canvas.height); //canvas 초기화
+        endRound();
+    
+        // score =  localStorage.getItem("score");
+        // //endRound();
+        // console.log(score);
+        // return 0;
     }
-};
- 
+}
